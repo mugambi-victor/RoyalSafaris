f1d97b14bd81a3abdfce143c7ed29c34501ab574
mugambi-victor <victormmugambi@gmail.com>
2023-07-28 15:19:47 +0300

    creating the app and started creating homepage layout

---
+++ src/Components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const Navbar = () => {
+  return (
+    <StyledNavbar>
+        <div className='logo'>
+            <h1>RoyalSafaris</h1>
+        </div>
+        <ul>
+        <li>Home</li>
+        <li>Events</li>
+        <li>Services</li>
+        <li>About</li>
+        </ul>
+    </StyledNavbar>
+  )
+}
+
+const StyledNavbar=styled.nav`
+display:flex;
+justify-content: space-between;
+ul{
+    margin-right:10px;
+    display:flex;
+    float:right;
+}
+li{
+    display:flex;
+    float:right;
+    padding: 13px;
+    color:white;
+}
+.logo{
+    padding:13px;
+    color:white;
+}
+`
+export default Navbar
---
+++ src/Components/Paragraph1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Paragraph1 = () => {
+  return (
+    <div>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.The quick brown fox jumped over the lazy dog.</div>
+  )
+}
